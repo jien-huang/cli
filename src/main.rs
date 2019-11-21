54f08d72a88289978f1b5acb23d6ac32660f0ec2
pangyazhi <pangyazhi@gmail.com>
2019-11-21 01:18:56 +0000

    add test

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,30 @@ use curl::easy::Easy;
 // Print a web page onto stdout
 fn main() {
     let mut easy = Easy::new();
-    easy.url("https://www.rust-lang.org/").unwrap();
+    get_url(&mut easy, "https://www.rust-lang.org/");
+
+    println!("{}", easy.response_code().unwrap());
+}
+
+fn get_url(easy: &mut Easy, url: &str) {
+    easy.url(url).unwrap();
     easy.write_function(|data| {
-        stdout().write_all(data).unwrap();
+//        stdout().write_all(data).unwrap();
         Ok(data.len())
     }).unwrap();
     easy.perform().unwrap();
+}
 
-    println!("{}", easy.response_code().unwrap());
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_get_url() {
+        let mut easy = Easy::new();
+        get_url(&mut easy, "https://www.google.com/");
+        assert!(easy.response_code().unwrap() > 0);
+    }
+
 }
